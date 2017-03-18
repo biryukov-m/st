015933bd6a43561a774c9cb67cc1df92266f21f3
biryukov-m <biryukov.mm@gmail.com>
2017-03-18 13:47:24 +0200

    1.5.11 added - namespaces in classes atributes and their exemplars 1.13, 2 - deleted

--- basic/module 1/1.13.py
+++
@@ -1,5 +0,0 @@
-def s(a, *vs, b=10):
-    print('a = ', a)
-    print('b = ', b)
-    print('vs =', vs)
-s(10)
--- basic/module 1/2.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-cnt = 0
-for i in range(n):
-    cnt += int(input())
-print(cnt)
