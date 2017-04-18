d88a8d31defe53cf86f459ef68558c3672e16bdd
biryukov-m <biryukov.mm@gmail.com>
2017-04-18 17:21:22 +0300

    Iterator and random iterator added

---
+++ basic/module 2/2.2.9.py
@@ -0,0 +1,14 @@
+lst = [1,2,3,4,5,6]
+book = {'title': 'Gondoleri',
+        'author': 'Papa Romanian XVI',
+        'year published': '1999'}
+string = 'Hola amigos!'
+
+# for i in book.items():
+#     print(i)
+
+iterator = iter(book)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
