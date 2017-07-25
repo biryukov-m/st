fb50ad0c37fd8fde4a3eb9b34c8b9d83cb527d2e
Biryukov <biryukov.mm@gmail.com>
2017-07-25 19:16:55 +0300

    Url parsing regular expression improved Output now works with 're.findall'  way

--- basic/module 3/3.4.7 html urlparse.py
+++ basic/module 3/3.4.7 html urlparse.py
@@ -1,11 +1,17 @@
 import requests
 import re
 
-# html = input().strip()
-html = 'https://www.ukr.net/'
-pattern = r'<a[^>]*?href="(https?.^/*?)"[^>]*?>'
+html = input().strip()
+# html = 'http://pastebin.com/raw/7543p0ns'
+# html = 'http://pastebin.com/raw/2mie4QYa'
+# html = 'https://www.ukr.net/'
+pattern = r'''<.*href\s*=\s*['"](?:[\w*-*]*?://)?(\w[_\w.-]*\w\w)(?:[/:\?\w]*)(?:\w*\.?\w*)?['"]'''
 res = requests.get(html).text
-urls = re.findall(pattern, res)
-urls.sort()
-for url in urls:
-    print(url)
+url = re.findall(pattern, res)
+out = []
+for u in url:
+    if u not in out:
+        out.append(u)
+out.sort()
+for u in out:
+    print(u)
